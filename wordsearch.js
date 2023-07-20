9e7054d092db2e707209e0b1369afeeae6b05ec6
Abdul Rahman <lhlrahman@gmail.com>
2023-07-20 19:02:02 -0400

    everything in working order, no edge cases checked

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let i of horizontalJoin) {
+    if (i.includes(word)) return true;
+  }
+  const verticalcheck = transpose(horizontalJoin).map(ls => ls.join(''));
 
-module.exports = wordSearch
+
+  for (let i of verticalcheck) {
+    if (i.includes(word)) return true;
+  }
+  return false;
+  
+};
+
+
+
+const transpose = function(matrix) {
+  let arr = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    const newRow = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newRow.push(matrix[j][i]);
+    }
+    arr.push(newRow);
+  }
+  console.log(arr);
+  return arr;
+  
+};
+
+module.exports = wordSearch;
